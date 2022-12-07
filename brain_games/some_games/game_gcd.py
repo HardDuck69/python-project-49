e903863e632bba368b92c0dc236461f03b7240d0
HardDuck69 <tmu1408@gmail.com>
2022-12-08 00:19:06 +0300

    upd game_calc.py

--- brain_games/some_games/game_gcd.py
+++ brain_games/some_games/game_gcd.py
@@ -6,8 +6,8 @@ RULE = 'Find the greatest common divisor of given numbers.'
 
 
 def get_game():
-    number1 = random.randint(1, 100)
-    number2 = random.randint(1, 100)
-    question = f"{number1} {number2}"
-    correct_answer = str(gcd(number1, number2))
+    random_number1 = random.randint(1, 100)
+    random_number2 = random.randint(1, 100)
+    question = f"{random_number1} {random_number2}"
+    correct_answer = str(gcd(random_number1, random_number2))
     return question, correct_answer
