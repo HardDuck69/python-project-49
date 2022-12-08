7808d04a76f84aa6cb2b0d58710ce36eed4eb26b
HardDuck69 <tmu1408@gmail.com>
2022-12-08 12:38:31 +0300

    upd game_logic.py

--- brain_games/game_logic.py
+++ brain_games/game_logic.py
@@ -1,19 +1,16 @@
 import prompt
 
 
-def round(question, correct_answer):
-    print(f'Question: {question}')
-    answer = prompt.string('Your answer: ')
-    if correct_answer == answer:
-        print('Correct!')
-        return True
-    else:
-        print(f"'{answer}' is wrong answer ;(. \
-Correct answer was '{correct_answer}'")
-        return False
-
-
 def start_game(game):
+    def round(question, correct_answer):
+        print(f'Question: {question}')
+        answer = prompt.string('Your answer: ')
+        if correct_answer == answer:
+            print('Correct!')
+            return True
+        print(f"'{answer}' is wrong answer ;(. \
+ Correct answer was '{correct_answer}'")
+        return False
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
