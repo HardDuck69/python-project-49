01a284ffea90d60b7c30e2b06ea2fb050ecc501f
HardDuck69 <tmu1408@gmail.com>
2022-12-08 11:00:46 +0300

    upd game_progression.py and cli.py

--- brain_games/games/game_progression.py
+++ brain_games/games/game_progression.py
@@ -5,13 +5,18 @@ RULE = 'What number is missing in the progression?'
 LENGHT = 9
 
 
-def get_game():
+def get_progression():
     random_number = randint(0, 10)
     step = randint(1, 10)
     progression = []
     for i in range(LENGHT):
         next_step = random_number + step * i
         progression.append(str(next_step))
+    return progression
+
+
+def get_game():
+    progression = get_progression()
     secret_index = choice(range(len(progression) - 1))
     correct_answer = progression[secret_index]
     progression[secret_index] = '..'
